af7320197b53d30958dc23b226a165b0db79f7d7
bella-cockrell <heybellac@gmail.com>
2020-05-08 15:46:45 +0100

    Adding HTML semantic tags

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Experience from './Pages/Experience';
 import SVGLogo from './Components/SVGLogo';
-import Skills from './Pages/Skills';
+import Skills from './Components/Skills';
 import Projects from './Pages/Projects';
-import AboutMe from './Pages/AboutMe';
-import Contact from './Pages/Contact';
+import AboutMe from './Components/AboutMe';
+import Contact from './Components/Contact';
 
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+
+      <header>
+        <h1> Hi, I'm Bella Cockrell </h1>
+      </header>
       <nav>
         Home
         Experience
         Projects
       </nav>
-
-      <header>
-        <h1> Hi, I'm Bella Cockrell </h1>
-      </header>
 
       <sidebar>
         <SVGLogo />
@@ -40,7 +40,7 @@ function App() {
       </article>
 
       <footer>
-        Hello, this is the footer
+        This is the footer
       </footer>
     </div >
   );
