5ccb104b7075042ba65ba66b01194d90db08869e
bella-cockrell <heybellac@gmail.com>
2020-05-08 14:16:26 +0100

    Adding About Me to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import Experience from './Pages/Experience';
+import AboutMe from './Pages/AboutMe';
 import Contact from './Pages/Contact';
 
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <h1> Hi, I'm Bella Cockrell </h1>
-      <h2>
-        About me
-      </h2>
-      <p>
-        I am a frontend dev with a keen spirit and an eye for design. I like
-        always to improve my knowledge by challenging myself and learn new
-        technologies.
-      </p>
       <article>
+        <AboutMe />
         <Contact />
       </article>
       <Experience />
