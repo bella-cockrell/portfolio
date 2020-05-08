4da4583918d0d0eda80455b4bc9c7250ca1cf729
bella-cockrell <heybellac@gmail.com>
2020-05-08 14:39:30 +0100

    Adding SVG component to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Experience from './Pages/Experience';
+import SVGLogo from './Components/SVGLogo';
 import Projects from './Pages/Projects';
 import AboutMe from './Pages/AboutMe';
 import Contact from './Pages/Contact';
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className='App'>
       <h1> Hi, I'm Bella Cockrell </h1>
+      <SVGLogo />
       <article>
         <AboutMe />
         <Contact />
