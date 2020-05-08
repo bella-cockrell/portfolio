f44ca884015dda56ff5f3e13f3bb6ccda939cacb
bella-cockrell <heybellac@gmail.com>
2020-05-08 12:28:09 +0100

    Adding Contact and Experience pages

---
+++ src/Pages/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>
+        Contact
+      </h2>
+      <p>
+        LinkedIn:
+        <a href='https://www.linkedin.com/in/bella-cockrell/' alt='bella-cockrell'>
+          bella-cockrell
+        </a>
+        Github:
+        <a href='https://github.com/bella-cockrell' alt='bella-cockrell'>
+          bella-cockrell
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/Pages/Experience.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Experience = () => {
+  return (
+    <div>
+      <h1>
+        Experience
+      </h1>
+      <h2>
+        Frontend dev at BoulderBook (April 2020 – present)
+      </h2>
+      <div>
+        Currently working full-time with a team of back- and
+        frontend developers creating an MVP that allows users to log their
+        climbing sessions. Technologies → React with Material UI and Styled
+        Components, HTML5, CSS3, Javascript, Typescript, Git.
+      </div>
+      <h2>
+        Editor at Pavilion Books (Nov 2014 – present)
+      </h2>
+      <div>
+        I currently project-manage the craft, art and gift titles under the
+        Pavilion and Collins & Brown imprints. I have extensive experience
+        with handling administration, managing good client relations,
+        proofreading and editing.
+      </div>
+      <h2>
+        Content Editor at Quintet (Sept – Nov 2014)
+      </h2>
+      <div>
+        I provided content for a new custom cookbook app that reused existing
+        recipe content. I worked closely with the app’s developers and
+        provided user feedback.
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
