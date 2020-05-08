b295c0a22caf86880747c0eab8a704ba53af4c33
bella-cockrell <heybellac@gmail.com>
2020-05-08 14:39:12 +0100

    Adding SVG logo

---
+++ src/Components/SVGLogo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from './logo.svg';
+
+const SVGLogo = () => {
+  return (
+    <img src={Logo} alt='a beautiful portait of me' />
+  );
+};
+
+export default SVGLogo;
