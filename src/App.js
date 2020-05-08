e76f1a816ae3f0ed5e7a381a896442c21bb69a89
bella-cockrell <heybellac@gmail.com>
2020-05-08 12:21:06 +0100

    Adding App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Experience from './Pages/Experience';
+import Contact from './Pages/Contact';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Hi, I'm Bella Cockrell </h1>
+      <h2>
+        About me
+      </h2>
+      <p>
+        I am a frontend dev with a keen spirit and an eye for design. I like
+        always to improve my knowledge by challenging myself and learn new
+        technologies.
+      </p>
+      <article>
+        <Contact />
+      </article>
+      <Experience />
     </div>
   );
 }
