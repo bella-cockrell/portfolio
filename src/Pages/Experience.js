f46cf33faa681fb146e9f294e6c288552cd28e34
bella-cockrell <heybellac@gmail.com>
2020-05-08 16:07:55 +0100

    Adding CSS import

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../App.css';
 
 const Experience = () => {
   return (
@@ -6,27 +7,27 @@ const Experience = () => {
       <h1>
         Experience
       </h1>
-      <h2>
+      <bold>
         Frontend dev at BoulderBook (April 2020 – present)
-      </h2>
+      </bold>
       <div>
         Currently working full-time with a team of back- and
         frontend developers creating an MVP that allows users to log their
         climbing sessions. Technologies → React with Material UI and Styled
         Components, HTML5, CSS3, Javascript, Typescript, Git.
       </div>
-      <h2>
+      <bold>
         Editor at Pavilion Books (Nov 2014 – present)
-      </h2>
+      </bold>
       <div>
         I currently project-manage the craft, art and gift titles under the
         Pavilion and Collins & Brown imprints. I have extensive experience
         with handling administration, managing good client relations,
         proofreading and editing.
       </div>
-      <h2>
+      <bold>
         Content Editor at Quintet (Sept – Nov 2014)
-      </h2>
+      </bold>
       <div>
         I provided content for a new custom cookbook app that reused existing
         recipe content. I worked closely with the app’s developers and
