862876ffb9e2b97f01519dd203724249a09d9b9d
bella-cockrell <heybellac@gmail.com>
2020-05-08 15:30:33 +0100

    Adding Skills component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Experience from './Pages/Experience';
 import SVGLogo from './Components/SVGLogo';
+import Skills from './Pages/Skills';
 import Projects from './Pages/Projects';
 import AboutMe from './Pages/AboutMe';
 import Contact from './Pages/Contact';
@@ -10,15 +11,38 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1> Hi, I'm Bella Cockrell </h1>
-      <SVGLogo />
+      <nav>
+        Home
+        Experience
+        Projects
+      </nav>
+
+      <header>
+        <h1> Hi, I'm Bella Cockrell </h1>
+      </header>
+
+      <sidebar>
+        <SVGLogo />
+        <article>
+          <AboutMe />
+        </article>
+        <article>
+          <Contact />
+        </article>
+        <article>
+          <Skills />
+        </article>
+      </sidebar>
+
       <article>
-        <AboutMe />
-        <Contact />
+        <Experience />
+        <Projects />
       </article>
-      <Experience />
-      <Projects />
-    </div>
+
+      <footer>
+        Hello, this is the footer
+      </footer>
+    </div >
   );
 }
 
