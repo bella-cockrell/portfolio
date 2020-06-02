83435afeb6bda26b9aa6ee43fe09f2479109e1f9
bella-cockrell <heybellac@gmail.com>
2020-06-02 17:54:37 +0100

    Adding Accordion component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import SVGLogo from './Components/SVGLogo';
 import './App.css';
-import Accordion from './Components/AccordionMenu/index';
+import Menu from './Components/AccordionMenu/Menu';
 
 function App() {
   return (
     <div className='App'>
-
       <header>
         <h1> Hi, I'm <span style={{ color: '#2a9d8f' }}>Bella Cockrell</span></h1>
         <SVGLogo />
       </header>
-      <Accordion />
+      <body>
+        <Menu />
+      </body>
       <footer>Made with Love</footer>
     </div>
   );
