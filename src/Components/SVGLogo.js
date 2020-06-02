6741a97e320d774bf97063989ee80a166e2695d8
bella-cockrell <heybellac@gmail.com>
2020-06-02 13:59:32 +0100

    Updating alt text

--- src/Components/SVGLogo.js
+++ src/Components/SVGLogo.js
@@ -3,7 +3,10 @@ import Logo from './logo.svg';
 
 const SVGLogo = () => {
   return (
-    <img src={Logo} alt='a beautiful portait of me' />
+    <img src={Logo}
+      alt='a cartoon portait of me'
+      style={{ width: '100%' }}
+    />
   );
 };
 
