2f1828d956ba5b3a0d3c6fa26bbbca74b72395f5
bella-cockrell <heybellac@gmail.com>
2020-06-02 13:47:44 +0100

    Adding Accordion Menu to App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React from 'react';
-// import Alert from './Components/Alert';
-import Experience from './Pages/Experience';
 import SVGLogo from './Components/SVGLogo';
-import Skills from './Components/Skills';
-import Projects from './Pages/Projects';
-import AboutMe from './Components/AboutMe';
-import Contact from './Components/Contact';
-
 import './App.css';
+import Accordion from './Components/AccordionMenu/index';
 
 function App() {
   return (
     <div className='App'>
-      {/* <Alert /> */}
-      <body>
-        <header>
-          <h1> Hi, I'm <span style={{ color: '#2a9d8f' }}>Bella Cockrell</span></h1>
-        </header>
-        <nav>
-        </nav>
 
-        <sidebar className='card'>
-          <SVGLogo />
-          <article>
-            <AboutMe />
-          </article>
-          <article>
-            <Contact />
-          </article>
-          <article>
-            <Skills />
-          </article>
-        </sidebar>
-
-        <article className='container'>
-          <Experience />
-          <Projects />
-        </article>
-
-        <footer />
-      </body>
-    </div >
+      <header>
+        <h1> Hi, I'm <span style={{ color: '#2a9d8f' }}>Bella Cockrell</span></h1>
+        <SVGLogo />
+      </header>
+      <Accordion />
+      <footer>Made with Love</footer>
+    </div>
   );
-}
+};
 
 export default App;
