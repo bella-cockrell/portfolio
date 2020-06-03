342780ec3ef45531955ab3ac4b8e85e6934d28f0
bella-cockrell <heybellac@gmail.com>
2020-06-03 18:06:28 +0100

    Styling header and image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import SVGLogo from './Components/SVGLogo';
+import Logo from './Components/logo.svg';
 import './App.css';
 import Menu from './Components/AccordionMenu/Menu';
 
 function App() {
   return (
     <div className='App'>
+
       <header>
         <h1> Hi, I'm <span style={{ color: '#2a9d8f' }}>Bella Cockrell</span></h1>
-        <SVGLogo />
+        <img src={Logo} alt='a cartoon portait of me' />
       </header>
+
       <body>
         <Menu />
       </body>
-      <footer>Made with Love</footer>
-    </div>
+
+      <footer>Check out my
+        <a style={{ paddingLeft: '0.4em' }}
+          href='https://github.com/bella-cockrell'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Github
+        </a>
+      </footer>
+
+    </div >
   );
 };
 
