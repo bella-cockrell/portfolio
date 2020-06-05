fb7d5464e69c66373bf65d8f668b75dc160a9343
bella-cockrell <heybellac@gmail.com>
2020-06-05 17:43:23 +0100

    Adding social media links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Logo from './Images/logo.svg';
 import './App.css';
 import Menu from './Components/AccordionMenu/Menu';
 
+import Github from './Images/github.svg';
+import LinkedIn from './Images/linkedin.svg';
+import Twitter from './Images/twitter.svg';
+
 function App() {
   return (
     <div className='App'>
@@ -12,18 +16,21 @@ function App() {
         <img src={Logo} alt='a cartoon portait of me' />
       </header>
 
+      <div className='contact-card'>
+          <a href='https://github.com/bella-cockrell' alt='Github' target='_blank' rel="noopener noreferrer">
+            <img src={Github} />
+          </a>
+          <a href='https://twitter.com/heybellac' alt='Twitter' target='_blank' rel="noopener noreferrer">
+            <img src={Twitter} />
+          </a>
+          <a href='https://www.linkedin.com/in/bella-cockrell/' alt='Linkedin' target='_blank' rel="noopener noreferrer">
+            <img src={LinkedIn} />
+          </a>
+      </div>
+
       <body>
         <Menu />
       </body>
-
-      <footer>Check out my
-        <a style={{ paddingLeft: '0.4em' }}
-          href='https://github.com/bella-cockrell'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Github
-        </a>
-      </footer>
 
     </div >
   );
